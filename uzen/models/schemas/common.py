3502f775324b8c5c0eaf31268343b72404f168fb
Manabu Niseki <manabu.niseki@gmail.com>
2020-04-09 16:48:32 +0900

    refactor: use ellipsis
    Use ellipsis in required fields

--- uzen/models/schemas/common.py
+++ uzen/models/schemas/common.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 class CountResponse(BaseModel):
     count: int = Field(
-        None,
+        ...,
         title="A number of matched items",
         description="A number of matched items with filters",
     )
