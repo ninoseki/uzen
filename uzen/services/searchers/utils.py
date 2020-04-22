be37b8a47f865aecad1ab7eb131800baf093cb92
Manabu Niseki <manabu.niseki@gmail.com>
2020-04-22 13:34:12 +0900

    refactor: use isinstance
    Use isinstance instead of type

--- uzen/services/searchers/utils.py
+++ uzen/services/searchers/utils.py
@@ -3,7 +3,7 @@ from typing import Union, cast
 
 
 def convert_to_datetime(d: Union[datetime, date]) -> datetime:
-    if type(d) == datetime:
+    if isinstance(d, datetime):
         return cast(datetime, d)
 
     return datetime.combine(cast(date, d), datetime.min.time())
