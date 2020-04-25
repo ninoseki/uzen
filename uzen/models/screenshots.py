089583590449dc694b7abaa691c921b783e288b9
Manabu Niseki <manabu.niseki@gmail.com>
2020-04-25 09:36:36 +0900

    refactor: use zlib to store screenshot data
    Use zlib to store screenshot data to improve performance

--- uzen/models/screenshots.py
+++ uzen/models/screenshots.py
@@ -1,3 +1,5 @@
+import base64
+import zlib
 from typing import Union
 
 from tortoise import fields
@@ -8,11 +10,25 @@ from uzen.schemas.screenshots import Screenshot as ScreenshotModel
 
 
 class Screenshot(AbstractBaseModel):
-    data = fields.TextField()
+    _data: str = fields.TextField(source_field="data")
 
     snapshot: fields.OneToOneRelation["Snapshot"] = fields.OneToOneField(
         "models.Snapshot", related_name="_screenshot", on_delete=fields.CASCADE
     )
+
+    @property
+    def data(self) -> str:
+        try:
+            b64decoded = base64.b64decode(self._data.encode())
+            decompressed = zlib.decompress(b64decoded)
+            return decompressed.decode()
+        except zlib.error:
+            return self._data
+
+    @data.setter
+    def data(self, data: str):
+        compressed = zlib.compress(data.encode())
+        self._data = base64.b64encode(compressed).decode()
 
     def to_model(self) -> Union[ScreenshotModel, BaseScreenshot]:
         if self.snapshot_id is not None:
