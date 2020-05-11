a6b51659dd1f7484df4f07166741959933e72f8c
Manabu Niseki <manabu.niseki@gmail.com>
2020-05-11 15:23:53 +0900

    chore: use mdi & fas via npm packages

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,10 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "@mdi/font/css/materialdesignicons.css";
+import "highlight.js/styles/github.css";
+
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
-
-import "highlight.js/styles/github.css";
 
 Vue.use(Buefy);
 
