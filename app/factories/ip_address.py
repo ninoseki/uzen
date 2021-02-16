f8f56befe7a0a31c81044f6c559a38f7bda11f0e
Manabu Niseki <manabu.niseki@gmail.com>
2021-02-16 18:19:58 +0900

    refactor: use aiometer

--- app/factories/ip_address.py
+++ app/factories/ip_address.py
@@ -1,3 +1,7 @@
+from functools import partial
+
+import aiometer
+
 from app import models, schemas
 from app.services.ip2asn import IP2ASN
 from app.services.whois import Whois
@@ -6,9 +10,12 @@ from app.services.whois import Whois
 class IPAddressFactory:
     @staticmethod
     async def from_ip_address(ip_address: str) -> schemas.IPAddress:
-        whois = await Whois.lookup(ip_address)
-        res = await IP2ASN.lookup(ip_address)
-        snapshots = await models.Snapshot.find_by_ip_address(ip_address)
+        tasks = [
+            partial(Whois.lookup, ip_address),
+            partial(IP2ASN.lookup, ip_address),
+            partial(models.Snapshot.find_by_ip_address, ip_address),
+        ]
+        whois, res, snapshots = await aiometer.run_all(tasks)
 
         asn = res.get("asn", "")
         country_code = res.get("country_code", "")
