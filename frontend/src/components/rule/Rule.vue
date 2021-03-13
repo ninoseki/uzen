6be2768f6ddd96d75c8f96f65d16bb714875e660
Manabu Niseki <manabu.niseki@gmail.com>
2021-03-13 15:48:19 +0900

    refactor: go back to the root when an error occurs [skip ci]

--- frontend/src/components/rule/Rule.vue
+++ frontend/src/components/rule/Rule.vue
@@ -1,94 +1,81 @@
 <template>
-  <div>
-    <Loading v-if="getRuleTask.isRunning"></Loading>
-    <Error
-      :error="getRuleTask.last.error.response.data"
-      v-else-if="getRuleTask.isError && getRuleTask.last !== undefined"
-    ></Error>
+  <div class="box">
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <H2>
+          {{ rule.name }}
+        </H2>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <router-link
+            class="button"
+            :to="{
+              name: 'EditRule',
+              params: { id: rule.id },
+            }"
+            >Edit
+          </router-link>
+        </div>
+      </div>
+    </nav>
 
-    <div
-      class="box"
-      v-if="getRuleTask.last && getRuleTask.last.value && !getRuleTask.isError"
-    >
-      <nav class="navbar">
-        <div class="navbar-brand">
-          <H2>
-            {{ getRuleTask.last.value.name }}
-          </H2>
-        </div>
-        <div class="navbar-menu">
-          <div class="navbar-end">
-            <router-link
-              class="button"
-              :to="{
-                name: 'EditRule',
-                params: { id: getRuleTask.last.value.id },
-              }"
-              >Edit
-            </router-link>
+    <div class="column is-full">
+      <div class="columns">
+        <div class="column is-half">
+          <div class="table-container">
+            <Table class="table">
+              <tbody>
+                <tr>
+                  <th>ID</th>
+                  <td>{{ rule.id }}</td>
+                </tr>
+                <tr>
+                  <th>Target</th>
+                  <td>{{ rule.target }}</td>
+                </tr>
+                <tr>
+                  <th>Created at</th>
+                  <td>{{ rule.createdAt || "N/A" }}</td>
+                </tr>
+                <tr>
+                  <th>Updated at</th>
+                  <td>{{ rule.updatedAt || "N/A" }}</td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
         </div>
-      </nav>
-
-      <div class="column is-full">
-        <div class="columns">
-          <div class="column is-half">
-            <div class="table-container">
-              <table class="table">
-                <tbody>
-                  <tr>
-                    <th>ID</th>
-                    <td>{{ getRuleTask.last.value.id }}</td>
-                  </tr>
-                  <tr>
-                    <th>Target</th>
-                    <td>{{ getRuleTask.last.value.target }}</td>
-                  </tr>
-                  <tr>
-                    <th>Created at</th>
-                    <td>{{ getRuleTask.last.value.createdAt || "N/A" }}</td>
-                  </tr>
-                  <tr>
-                    <th>Updated at</th>
-                    <td>{{ getRuleTask.last.value.updatedAt || "N/A" }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div class="column is-half">
-            <H3>Source</H3>
-            <pre><code class="yara">{{ getRuleTask.last.value.source || "N/A" }}</code></pre>
-          </div>
+        <div class="column is-half">
+          <H3>Source</H3>
+          <pre><code class="yara">{{ rule.source || "N/A" }}</code></pre>
         </div>
-        <div class="column">
-          <H3>
-            Recent related snapshots
-            <Counter :ruleId="getRuleTask.last.value.id" />
-          </H3>
-          <Table
-            v-if="hasSnapshots(getRuleTask.last.value)"
-            v-bind:snapshots="getRuleTask.last.value.snapshots"
-          />
-          <p v-else>N/A</p>
-        </div>
+      </div>
+      <div class="column">
+        <H3>
+          Recent related snapshots
+          <Counter :ruleId="rule.id" />
+        </H3>
+        <SnapshotTable v-if="hasSnapshots" v-bind:snapshots="rule.snapshots" />
+        <p v-else>N/A</p>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "@vue/composition-api";
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  PropType,
+} from "@vue/composition-api";
 import { useTitle } from "@vueuse/core";
-import { useAsyncTask } from "vue-concurrency";
 
-import { API } from "@/api";
 import Counter from "@/components/match/Counter.vue";
-import Table from "@/components/snapshot/TableWithScreenshot.vue";
-import Error from "@/components/ui/Error.vue";
+import SnapshotTable from "@/components/snapshot/TableWithScreenshot.vue";
 import H2 from "@/components/ui/H2.vue";
 import H3 from "@/components/ui/H3.vue";
-import Loading from "@/components/ui/Loading.vue";
 import { Rule } from "@/types";
 import { highlightCodeBlocks } from "@/utils/highlight";
 
@@ -96,42 +83,32 @@ export default defineComponent({
   name: "Rule",
   components: {
     Counter,
-    Error,
     H2,
     H3,
-    Loading,
-    Table,
+    SnapshotTable,
   },
   props: {
-    ruleId: {
-      type: String,
+    rule: {
+      type: Object as PropType<Rule>,
       required: true,
     },
   },
   setup(props, context) {
-    const getRuleTask = useAsyncTask<Rule, []>(async () => {
-      return await API.getRule(props.ruleId);
-    });
-
     const updateTitle = (ruleName: string): void => {
       useTitle(`${ruleName} - Uzen`);
     };
 
-    const getRule = async () => {
-      const rule = await getRuleTask.perform();
-      updateTitle(rule.name);
-    };
-
-    const hasSnapshots = (rule: Rule): boolean => {
-      return (rule?.snapshots || []).length > 0;
-    };
+    const hasSnapshots = computed(() => {
+      return (props.rule.snapshots || []).length > 0;
+    });
 
     onMounted(async () => {
-      await getRule();
+      updateTitle(props.rule.name);
+
       highlightCodeBlocks(context);
     });
 
-    return { getRuleTask, hasSnapshots };
+    return { hasSnapshots };
   },
 });
 </script>
