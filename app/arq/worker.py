4ace19a3cfe75414f6e01987f2270243150d64f4
Manabu Niseki <manabu.niseki@gmail.com>
2021-07-04 09:41:44 +0900

    refactor: set timeout [skip ci]

--- app/arq/worker.py
+++ app/arq/worker.py
@@ -1,10 +1,19 @@
+from arq.worker import func
 from tortoise import Tortoise
 
+from app.arq.constants import (
+    ENRICH_SNAPSHOT_TASK_NAME,
+    PREVIEW_TASK_NAME,
+    SIMILARITY_SCAN_TASK_NAME,
+    SNAPSHOT_TASK_NAME,
+    YARA_SCAN_TASK_NAME,
+)
 from app.arq.settings import get_redis_settings
 from app.arq.tasks.preview import preview_task
 from app.arq.tasks.similarity import similarity_scan_task
 from app.arq.tasks.snapshot import enrich_snapshot_task, take_snapshot_task
 from app.arq.tasks.yara import yara_scan_task
+from app.cache.constants import ONE_DAY, ONE_HOUR
 from app.database import init_db
 
 
@@ -17,12 +26,23 @@ async def shutdown(ctx: dict) -> None:
 
 
 class ArqWorkerSettings:
+    # default timeout = 300s, keep_result = 3600s
     functions = [
-        enrich_snapshot_task,
-        preview_task,
-        similarity_scan_task,
-        take_snapshot_task,
-        yara_scan_task,
+        func(enrich_snapshot_task, name=ENRICH_SNAPSHOT_TASK_NAME),
+        func(preview_task, name=PREVIEW_TASK_NAME),
+        func(
+            similarity_scan_task,
+            name=SIMILARITY_SCAN_TASK_NAME,
+            timeout=ONE_HOUR,
+            keep_result=ONE_DAY,
+        ),
+        func(take_snapshot_task, name=SNAPSHOT_TASK_NAME),
+        func(
+            yara_scan_task,
+            name=YARA_SCAN_TASK_NAME,
+            timeout=ONE_HOUR,
+            keep_result=ONE_DAY,
+        ),
     ]
     redis_settings = get_redis_settings()
 
