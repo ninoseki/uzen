a6aeb753e40f9b9c67c51adee92d8545e3fdb996
Manabu Niseki <manabu.niseki@gmail.com>
2021-12-11 14:40:48 +0900

    refactor: cache status in session storage

--- frontend/src/components/snapshot/Status.vue
+++ frontend/src/components/snapshot/Status.vue
@@ -1,16 +1,14 @@
 <template>
-  <p
-    class="has-text-grey-light has-text-right"
-    v-if="getStatusTask.last?.value"
-  >
+  <p class="has-text-grey-light has-text-right">
     You are going to take a snapshot from
-    {{ countryCodeToEmoji(getStatusTask.last.value.countryCode) }}
+    {{ countryCodeToEmoji(statusState.countryCode) }}
   </p>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
 import { useAsyncTask } from "vue-concurrency";
+import { useSessionStorage } from "@vueuse/core";
 
 import { API } from "@/api";
 import { Status } from "@/types";
@@ -19,17 +17,27 @@ import { countryCodeToEmoji } from "@/utils/country";
 export default defineComponent({
   name: "Status",
   setup() {
+    const statusState = useSessionStorage("uzen-status", {
+      ipAddress: "",
+      countryCode: "",
+    });
+
     const getStatusTask = useAsyncTask<Status, []>(async () => {
       return API.getStatus();
     });
 
     onMounted(async () => {
-      await getStatusTask.perform();
+      if (statusState.value.ipAddress === "") {
+        const status = await getStatusTask.perform();
+
+        statusState.value.ipAddress = status.ipAddress;
+        statusState.value.countryCode = status.countryCode;
+      }
     });
 
     return {
       countryCodeToEmoji,
-      getStatusTask,
+      statusState,
     };
   },
 });
