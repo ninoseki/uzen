fe5dcddfd75e87c0cc603b10833dcf67c7bd99f2
Manabu Niseki <manabu.niseki@gmail.com>
2022-01-03 17:00:43 +0900

    fix: update Censys link

--- frontend/src/links/censys.ts
+++ frontend/src/links/censys.ts
@@ -7,13 +7,13 @@ export class Censys implements Link {
   public type: LinkType;
 
   public constructor() {
-    this.baseURL = "https://censys.io";
+    this.baseURL = "https://search.censys.io";
     this.favicon = "https://www.google.com/s2/favicons?domain=censys.io";
     this.name = "Censys (IP)";
     this.type = "ip_address";
   }
 
   public href(hostname: string): string {
-    return this.baseURL + `/ipv4/${hostname}`;
+    return this.baseURL + `/hosts/${hostname}`;
   }
 }
